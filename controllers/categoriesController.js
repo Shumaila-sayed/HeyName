b31b6a5706126d019779475aa27bbb9f074e3895
Shumaila <sayedshumaila27@gmail.com>
2025-07-14 02:29:00 +0530

    structure category view

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -2,8 +2,8 @@ const db = require('../db/queries');
 
 const getCategories = async (req, res) => {
     try {
-        const categories = await db.getAllCategories();
-        res.send(categories);
+        const categories = await db.getAllCategories();        
+        res.render('categories', { categories: categories})
     } catch (error) {
         console.log(`error fetching categories: `, error);
         res.status(500).send("error fetching categories")
