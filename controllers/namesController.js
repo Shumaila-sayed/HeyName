e07bc5db111b6035c2b41869ba478b0e35bf3509
Shumaila <sayedshumaila27@gmail.com>
2025-07-18 19:18:43 +0530

    Correctly redirect user to the specified routes

--- controllers/namesController.js
+++ controllers/namesController.js
@@ -3,7 +3,8 @@ const db = require('../db/queries');
 const getNames = async (req, res) => {
 	try {
 		const names = await db.getAllNames();
-		res.send(names);
+		const categories = await db.getAllCategories();
+		res.render('names', { names: names, categories: categories, category_name: "", category_id: 0 });
 	} catch (error) {
 		console.log(`error fetching names: `, error);
 		res.status(500).send('error fetching names');
@@ -12,13 +13,19 @@ const getNames = async (req, res) => {
 
 const newNamePost = async (req, res) => {
 	try {
-		const {category_id, name, meaning, selectedCategories,  } = req.body;
+		const { name, meaning, selectedCategories,  } = req.body;
 		if (!name || !meaning || !selectedCategories) {
 			res.status(404).send('Name, Meaning or Category Not Found');
 			return;
 		}
+
+		const category_id = parseInt(req.body.category_id || 0);
 		await db.insertName(name, meaning, selectedCategories);
-		res.redirect(`/categories/${category_id}/names`);
+		if (category_id === 0) {
+			res.redirect('/names');
+		} else if (category_id > 0){
+			res.redirect(`/categories/${category_id}/names`);
+		}
 	} catch (error) {
 		console.log(`error adding name: `, error);
 		res.status(500).send('error adding name');
@@ -29,12 +36,17 @@ const updateName = async (req, res) => {
 	try {
 		const id = parseInt(req.params.id);
 		const { name, meaning, selectedCategories } = req.body;
+		const category_id = parseInt(req.body.category_id || 0);
 		if (!name || !meaning || !selectedCategories) {
 			res.status(404).send('Name, Meaning or Category Not Found');
 			return;
 		} 
 		await db.editName(id, name, meaning, selectedCategories);
-		res.redirect('/categories');
+		if (category_id === 0) {
+			res.redirect('/names');
+		} else {
+			res.redirect(`/categories/${category_id}/names`);
+		}
 	} catch (error) {
 		console.log('Error updating name: ' + error);
 		res.status(500).send('Error updating name');		
@@ -44,9 +56,13 @@ const updateName = async (req, res) => {
 const deleteName = async (req, res) => {
 	try {
 		const id = parseInt(req.params.id);
-		const { category_id } = req.body;
+		const category_id = parseInt(req.body.category_id || 0);
 		await db.deleteName(id);
-		res.redirect(`/categories/${category_id}/names`);
+		if (category_id === 0) {
+			res.redirect('/names');
+		} else {
+			res.redirect(`/categories/${category_id}/names`);
+		}
 	} catch (error) {
 		console.log('Error deleting name: ', error);
 		res.status(500).send('Error deleting name');		
